b5be9b60fa19b0130753d2828b6b3b6a7c8193e0
TDCIAN <tdcian71@gmail.com>
2022-01-24 23:14:55 +0900

    add textfieldcell

---
+++ UploadGoodsRxSwift/MainViewController/TitleTextFieldCell/TitleTextFieldCell.swift
@@ -0,0 +1,8 @@
+//
+//  TitleTextFieldCell.swift
+//  UploadGoodsRxSwift
+//
+//  Created by JeongminKim on 2022/01/24.
+//
+
+import Foundation
---
+++ UploadGoodsRxSwift/MainViewController/TitleTextFieldCell/TitleTextFieldCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TitleTextFieldCellViewModel.swift
+//  UploadGoodsRxSwift
+//
+//  Created by JeongminKim on 2022/01/24.
+//
+
+import RxCocoa
